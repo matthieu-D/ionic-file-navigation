3a4d240e82659b4d8bc7878053ce339b93a77275
Matthieu Drula <matthieu.drula.hs@gmail.com>
2017-08-12 17:58:36 -0400

    all working

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,75 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component , NgZone } from '@angular/core';
+import { Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  savedParentNativeURLs = [];
+  items;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public plt: Platform, public ngZone: NgZone) {
 
+    plt.ready()
+      .then(() => {
+        this.listRootDir();
+      })
   }
 
+  listRootDir = () => {
+
+    const ROOT_DIRECTORY = "file:///";
+
+    (<any> window).resolveLocalFileSystemURL(ROOT_DIRECTORY,
+      (fileSystem) => {
+
+        var reader = fileSystem.createReader();
+        reader.readEntries(
+          (entries) => {
+            this.ngZone.run(()=> {
+              this.items = entries;
+            });
+          }, this.handleError);
+      }, this.handleError);
+  }
+
+  goDown = (item) => {
+
+    let childName = this.items[0].name;
+    let childNativeURL = this.items[0].nativeURL;
+
+    const parentNativeURL = childNativeURL.replace(childName, '');
+
+    this.savedParentNativeURLs.push(parentNativeURL);
+
+    var reader = item.createReader();
+
+    reader.readEntries(
+      (children) => {
+        this.ngZone.run(()=> {
+          this.items = children;
+        })
+      }, this.handleError);
+  }
+
+  goUp = () => {
+
+    const parentNativeURL = this.savedParentNativeURLs.pop();
+
+    (<any> window).resolveLocalFileSystemURL(parentNativeURL,
+      (fileSystem) => {
+        var reader = fileSystem.createReader();
+        reader.readEntries(
+          (entries) => {
+            this.ngZone.run(()=> {
+              this.items = entries;
+            })
+          }, this.handleError);
+      }, this.handleError);
+  }
+
+  handleError = (error) => {
+    console.log('error reading,', error)
+  };
 }
